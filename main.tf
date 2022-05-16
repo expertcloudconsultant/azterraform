0016f19b9fcf06b9b5ff8e426486bd5c3a5c7b96
Samuel O <samuel.oppong@labs.com>
2022-05-16 19:04:00 +0100

    Another 17052022

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@
 
 variable "firstvar" {
     type = string
-    default = "hello nana esther"
+    default = "hello nana esther momo"
 
+}
+
+variable "mymap" {
+    type = map(string)
+    default = {
+      mykey = "this is my value"
+    }
 }
