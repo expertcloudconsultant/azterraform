11a85993976baeb7eb07f367cbb6824d9ab754ab
Samuel O <samuel.oppong@labs.com>
2022-05-20 18:21:07 +0100

    Linux VM Build 200522

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -19,3 +19,41 @@ provider "registry.terraform.io/hashicorp/azurerm" {
     "zh:eecb9fd0e7509c7fd4763e546ef0933f125770cbab2b46152416e23d5ec9dd53",
   ]
 }
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.2.0"
+  hashes = [
+    "h1:IHB49Mm7q/npgP6opLob2HR2AfdTu0fySYLAfrNNO38=",
+    "zh:2960977ce9a7d6a7d3e934e75ec5814735626f95c186ad95a9102344a1a38ac1",
+    "zh:2fd012abfabe7076f3f2f402eeef4970e20574d20ffec57c162b02b6e848c32f",
+    "zh:4cd3234671cf01c913023418b227eb78b0659f2cd2e0b387be1f0bb607d29889",
+    "zh:52e695b4fa3fae735ffc901edff8183745f980923510a744db7616e8f10dc499",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:848b4a294e5ba15192ee4bfd199c07f60a437d7572efcd2d89db036e1ebc0e6e",
+    "zh:9d49aa432a05748a9527e95448cebee1238c87c97c7e8dec694bfd709683f9c7",
+    "zh:b4ad4cf289d3f7408649b74b8639918833613f2a1f3cf51b51f4b2fdaa412dd2",
+    "zh:c1544c4b416096fb8d8dbf84c4488584a2844a30dd533b957e9e9e60a165f24e",
+    "zh:dc737d6b4591cad8c9a1d0b347e587e846d8d901789b29b4dd401b6cdf82c017",
+    "zh:f5645fd39f749dbbf847cbdc87ba0dbd141143f12917a6a8904faf8a9b64111e",
+    "zh:fdedf610e0d020878a8f1fedda8105e0c33a7e23c4792fca54460685552de308",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version = "3.4.0"
+  hashes = [
+    "h1:RS6P09hCyDYlqbK80SlnKMuqYg4MEvw+xyIf9Ca8WPM=",
+    "zh:2442a0df0cfb550b8eba9b2af39ac06f54b62447eb369ecc6b1c29f739b33bbb",
+    "zh:3ebb82cacb677a099de55f844f0d02886bc804b1a2b94441bc40fabcb64d2a38",
+    "zh:436125c2a7e66bc62a4a7c68bdca694f071d7aa894e8637dc83f4a68fe322546",
+    "zh:5f03db9f1d77e8274ff4750ae32d5c16c42b862b06bcb0683e4d733c8db922e4",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8190142ae8a539ab34193b7e75da0fa04035d1dcd8af8be94df1eafeeffb44b6",
+    "zh:8cdc7cd9221e27c189e5beaf78462fce4c2edb081f415a1eafc6da2949de31e2",
+    "zh:a5de0f7f5d63c59ebf61d3c1d94040f410665ff0aa04f66674efe24b39a11f94",
+    "zh:a9fce48db3c140cc3e06f8a3c7ef4d36735e457e7660442d6d5dcd2b0781adc3",
+    "zh:beb92de584c790c7c7f047e45ccd22b6ee3263c7b5a91ae4d6882ae6e7700570",
+    "zh:f373f8cc52846fb513f44f468d885f722ca4dc22af9ff1942368cafd16b796b3",
+    "zh:f69627fd6e5a920b17ff423cdbad2715078ca6d13146dc67668795582ab43748",
+  ]
+}
--- instance.tf
+++ instance.tf
@@ -1,23 +1,16 @@
-
-resource "azurerm_virtual_machine" "emc-eus-corporate-webserver-vm-01" {
-  name                  = "${var.prefix}-webserver-vm-01"
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "emc-eus-corporate-webserver-vm-01" {
+  name                  = "emc-eus-corporate-webserver-vm-01"
   location              = azurerm_resource_group.emc-eus-corporate-resources-rg.location
   resource_group_name   = azurerm_resource_group.emc-eus-corporate-resources-rg.name
-  network_interface_ids = [azurerm_network_interface.emc-eus-corporate-nic-01.id]
-  vm_size               = "Standard_DC1_v3"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  network_interface_ids = [azurerm_network_interface.corporate-webserver-vm-01-nic.id]
+  size                  = "Standard_DS1_v2"
 
   os_disk {
-    name                 = "webservervm01"
+    name                 = "corpwebservervm01disk"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
-
 
   source_image_reference {
     publisher = "Canonical"
@@ -25,37 +18,13 @@ resource "azurerm_virtual_machine" "emc-eus-corporate-webserver-vm-01" {
     sku       = "20.04-LTS"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "emcosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "emc-vm-host"
-    admin_username = "emcsvrusr"
-    admin_password = "Tassword1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "Production"
-  }
-}
 
+  computer_name                   = "corporate-webserver-vm-01"
+  admin_username                  = "linuxsrvuser"
+  disable_password_authentication = true
 
-# Create network interface
-resource "azurerm_network_interface" "emc-eus-corporate-nic-01" {
-  name                = "emc-eus-corporate-nic-01"
-  location            = azurerm_resource_group.emc-eus-corporate-resources-rg.location
-  resource_group_name = azurerm_resource_group.emc-eus-corporate-resources-rg.name
-
-  ip_configuration {
-    name                          = "${var.prefix}-vmnic"
-    subnet_id                     = azurerm_subnet.presentation-subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.emc-eus-corporate-nic-01-pip.id
-
+  admin_ssh_key {
+    username   = "linuxsrvuser"
+    public_key = tls_private_key.linuxsrvuserprivkey.public_key_openssh
   }
 }
