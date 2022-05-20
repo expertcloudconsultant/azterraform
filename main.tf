5bfd39e5b7d255a620a2f0b70dc907538703b35b
Samuel O <samuel.oppong@labs.com>
2022-05-20 20:18:10 +0100

    Build Linux Virtual Machine on Azure with Terraform

--- main.tf
+++ main.tf
@@ -36,13 +36,7 @@ resource "azurerm_subnet" "data-access-subnet" {
   address_prefixes     = ["172.20.2.0/24"]
 }
 
-#Create Public IP Address
-resource "azurerm_public_ip" "emc-eus2-corporate-nic-01-pip" {
-  name                = "emc-eus2-corporate-nic-01-pip"
-  location            = azurerm_resource_group.emc-eus2-corporate-resources-rg.location
-  resource_group_name = azurerm_resource_group.emc-eus2-corporate-resources-rg.name
-  allocation_method   = "Dynamic"
-}
+
 
 # Create public IPs
 resource "azurerm_public_ip" "corporate-webserver-vm-01-ip" {
@@ -100,6 +94,17 @@ resource "random_id" "randomId" {
 
   byte_length = 8
 }
+
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "corpwebservervm01storage" {
+  name                     = "diag${random_id.randomId.hex}"
+  location                 = azurerm_resource_group.emc-eus2-corporate-resources-rg.location
+  resource_group_name      = azurerm_resource_group.emc-eus2-corporate-resources-rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 
 # Create (and display) an SSH key
 resource "tls_private_key" "linuxsrvuserprivkey" {
