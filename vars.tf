6f3082cfb95068b4afe6291fa584524a05871a60
Samuel O <samuel.oppong@labs.com>
2022-05-28 04:57:29 +0100

    Output Added to Terraform IaC

--- vars.tf
+++ vars.tf
@@ -29,3 +29,19 @@ description = "dedicated ssh port for webserver shell access"
 default = 22
 
 }
+
+
+
+#Use Output to Find Information
+
+output "emc-vm-01-nic" {
+
+  value = "${azurerm_network_interface.corporate-webserver-vm-01-nic}"
+}
+
+
+output "tls-private-key" {
+
+  value = "${tls_private_key.linuxsrvuserprivkey}"
+  sensitive = true
+}
